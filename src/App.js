eda3450e8d76e4851042582e7ede866481418e08
Aadil Shaikh <aadilshaikh@kil-maclap-1.local>
2021-12-20 01:07:20 -0400

    Created basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import TodoList from './TodoList';
+import { useRef, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 function App() {
+
+  const [todos,setTodos] = useState([]);
+  
+  const reference = useRef();
+  const handleAddTodo = () => {
+    let newTodo = {id:uuidv4, name:reference.current.value, completed:false}
+    const prevTodos = [...todos,newTodo]
+    setTodos(prevTodos);
+    console.log(reference.current.value);
+    reference.current.value = "";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <TodoList todos={todos}/>
+    <input type="text" ref={reference}></input>
+    <button onClick={handleAddTodo}>Add Todo</button>
+    <button>Clear completed todos</button>
+    </>
+  )
 }
 
 export default App;
