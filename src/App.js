520155a3cdc580ac7cf00fa3f8e177f52f96adaa
Aadil Shaikh <aadilshaikh@kil-maclap-1.local>
2021-12-22 10:06:46 -0400

    Clear todos done

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import { v4 as uuidv4 } from 'uuid';
 function App() {
 
   const [todos,setTodos] = useState([]);
+  const [count,setCount] = useState(0);
   
   const reference = useRef();
   const handleAddTodo = () => {
     let newTodo = {id:uuidv4, name:reference.current.value, completed:false}
     const prevTodos = [...todos,newTodo]
     setTodos(prevTodos);
+    setCount(count+1);
     console.log(reference.current.value);
     reference.current.value = "";
+  }
+  const handleRemoveTodo = () => {
+    setTodos([]);
+    setCount(0);
   }
   return (
     <>
     <TodoList todos={todos}/>
     <input type="text" ref={reference}></input>
     <button onClick={handleAddTodo}>Add Todo</button>
-    <button>Clear completed todos</button>
+    <button onClick={handleRemoveTodo}>Clear completed todos</button>
+    <p>{count} left to do</p>
     </>
   )
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+
 export default function Todo({todo}) {
+    const fn1 = () => {
+        todo.completed = true
+    }
+    
     return (
         <div>
             {todo.name}
-            <input type="checkbox" checked={todo.completed} onChange={!todo.completed}></input>
+            <input type="checkbox" checked={todo.completed} onChange={!todo.completed} onClick={fn1}></input>
         </div>
     )
 }
